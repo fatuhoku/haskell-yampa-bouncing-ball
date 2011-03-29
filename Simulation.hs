313c698a430bd8c0f5e5999dee79241c8f48ec20
Hok Shun Poon <fushunpoon@googlemail.com>
2011-03-29 21:44:22 +0100

    Added bounds check for 4 sides

--- Simulation.hs
+++ Simulation.hs
@@ -10,10 +10,10 @@ import Types
 
 type COR     = GLfloat
 type Radius  = GLfloat
-type Ground  = GLfloat
 type Acc2    = (GLfloat,GLfloat)
 type Vel2    = (GLfloat,GLfloat)
 type Pos2    = (GLfloat,GLfloat)
+type Dir2    = (GLfloat,GLfloat)
 
 -- Top level signal function arrow
 simulate = discardInputs >>> update >>> draw
@@ -28,6 +28,14 @@ defPhysics = PhyC {
   cor = 0.8
 }
 
+data Bounds = Bounds {
+  xMin  :: GLfloat,
+  yMin  :: GLfloat,
+  xMax  :: GLfloat,
+  yMax  :: GLfloat
+}
+
+
 -- The boolean event says whether there has been a 'pat' on the ball or not.
 -- Right now, I don't want to complicate this.
 discardInputs :: SF (Event ()) ()
@@ -38,19 +46,19 @@ discardInputs = arr $ const ()
 -- TODO Abstract out the initial state.
 update :: SF () SceneNode
 update = proc () -> do
-  (pos,vel) <- bouncingBall (0,200) zeroVector (cor defPhysics) ballRadius ground -< ()
+  (pos,vel) <- bouncingBall (0,200) zeroVector (cor defPhysics) ballRadius bounds -< ()
   let ballSn = SnBall { ballAng = 0,  
                         ballPos = pos,
                         ballRad = ballRadius,
                         ballCol = ballColour }
   returnA  -< SnMulti [ballSn,groundSn]
   where
+    bounds = Bounds { xMin = -400, yMin = 50, xMax = 400, yMax = 600 }
     ballRadius = 30
     ballColour = Color3 (0.7 :: GLfloat) (0.7) (0.7)
-    ground = 40
     groundSn = SnRect {
       rectColor = Color3 (0.0 :: GLfloat) (0.7) (0.0),
-      rectTop = ground
+      rectTop = yMin bounds
     }
 
 -- fallingBall pos0 vel0
@@ -63,22 +71,32 @@ fallingBall p0 v0 = proc () -> do
 
 -- fallingBall' pos0 vel0 ground
 -- This time around, we detect for collisions with the ground given by 'ground',
--- and record the position and velocity at the location of the collision.
-fallingBall' :: Pos2 -> Vel2 -> Radius -> Ground ->
-                SF () ((Pos2, Vel2), Event (Pos2,Vel2))
-fallingBall' p0 v0 rad ground = proc () -> do
-  pv@(p,_) <- fallingBall p0 v0 -< ()
-  hit      <- edge              -< snd p <= ground + rad
-  returnA -< (pv, hit `tag` pv)
+-- and record the position, velocity, and the direction normal to the 
+-- at the location of the collision.
+-- Carries out a check for each of the four bounds
+fallingBall' :: Pos2 -> Vel2 -> Radius -> Bounds ->
+                SF () ((Pos2, Vel2), Event (Dir2,(Pos2,Vel2)))
+fallingBall' p0 v0 rad bounds = proc () -> do
+  pv@(p,v) <- fallingBall p0 v0 -< ()
+  hitXMin  <- edgeTag (1, 0) -< fst p <= xMin bounds + rad
+  hitYMin  <- edgeTag (0, 1) -< snd p <= yMin bounds + rad
+  hitXMax  <- edgeTag (-1,0) -< fst p >= xMax bounds - rad
+  hitYMax  <- edgeTag (0,-1) -< snd p >= yMax bounds - rad
+  let hitInfo = foldr1 (mergeBy mergeHits) [hitXMin,hitYMin,hitXMax,hitYMax]
+  returnA -< (pv, hitInfo `attach` pv)
+  where
+    mergeHits = (^+^) -- simply add the two collision directions together.
 
 
 -- We use the extra collision information as given by fallingBall' to compute
 -- the new bouncing ball's velocity.
-bouncingBall :: Pos2 -> Vel2 -> COR -> Radius -> Ground -> SF () (Pos2,Vel2)
-bouncingBall p0 v0 cor rad ground = bouncingBall' p0 v0
+-- Bounces off all four sides of the screen!
+bouncingBall :: Pos2 -> Vel2 -> COR -> Radius -> Bounds -> SF () (Pos2,Vel2)
+bouncingBall p0 v0 cor rad bounds = bouncingBall' p0 v0
   where
     bouncingBall' p0 v0 =
-      switch (fallingBall' p0 v0 rad ground) $ \(p,v) -> bouncingBall' p ((-cor)*^v)
+      switch (fallingBall' p0 v0 rad bounds) $
+        \(dir,(p,v)) -> bouncingBall' p ((-cor)*^v)
       
 
 -- Renders the scene using OpenGL from the Render module
